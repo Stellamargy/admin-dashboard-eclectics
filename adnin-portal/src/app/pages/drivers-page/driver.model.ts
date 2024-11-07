1c0e1b526e13a732869d9941e8e0ce56cadd8590
stlmargy <stellahmargy0211@gmail.com>
2024-11-07 16:58:28 +0300

    clients table and its features

---
+++ adnin-portal/src/app/pages/drivers-page/driver.model.ts
@@ -0,0 +1,31 @@
+export interface Driver {
+    "id":number,
+    "status": string,
+    "latitude": number,
+    "serviceName":string,
+    "longitude": number,
+    "active": boolean,
+    "fullName": string,
+    "email": "string",
+    "phoneNumber": string,
+    "dateOfBirth": string,
+    "available": boolean,
+    "password": string,
+    "profilePictureUrl":string,
+    "role": string,
+    "vehicleType": string,
+    "vehicleModel": string,
+    "vehicleRegistrationNumber": string,
+    "vehicleMake": string,
+    "insuranceDetails": string,
+    "licenseNumber": string,
+    "licenseClass": string,
+    "drivingExperience": number,
+    "criminalBackgroundCheck": boolean,
+    "mpesaNumber": string,
+    "bankAccountDetails": string,
+    "paymentPreference": string,
+    "termsAndConditionsAgreed": boolean
+  
+}
+  
--- adnin-portal/src/app/pages/drivers-page/drivers-table-data/drivers-table-data.component.ts
+++ adnin-portal/src/app/pages/drivers-page/drivers-table-data/drivers-table-data.component.ts
@@ -1,73 +1,14 @@
-import { Component ,ViewChild, AfterViewInit} from '@angular/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { CommonEngine } from '@angular/ssr';
-import { CommonModule } from '@angular/common';
-
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
-
-interface DriverFilterOption {
-  value: string;
-  viewValue: string;
-}
-interface DriverFilterGroup {
-  disabled?: boolean;
-  name: string;
-  filterOptions: DriverFilterOption[];
-}
-
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-drivers-table-data',
   standalone: true,
-  imports: [ MatInputModule,MatFormFieldModule,MatSelectModule, ReactiveFormsModule,CommonModule],
+  imports: [],
   templateUrl: './drivers-table-data.component.html',
   styleUrl: './drivers-table-data.component.css'
 })
 export class DriversTableDataComponent {
-   driverFilterGroups: DriverFilterGroup[] = [
-    {
-      name: 'Status',
-      filterOptions: [
-        {value: 'pending', viewValue: 'Pending'},
-        {value: 'approved', viewValue: 'Approved'},
-        {value: 'rejected', viewValue: 'Rejected'},
-      ],
-    },
-    {
-      name: 'Availability',
-      filterOptions: [
-        {value: 'true', viewValue: 'Available'},
-        {value: 'false', viewValue: 'Unavailable'},
-      ],
-    },
-    {
-      name: 'Role',
-      filterOptions: [
-        {value: 'passenger', viewValue: 'Passenger'},
-        {value: 'cargo', viewValue: 'Cargo'},
-        {value: 'mover', viewValue: 'Mover'},
-      ],
-    },
-    {
-      name: 'Driving Experience',
-      filterOptions: [
-        {value: 'less-than-5', viewValue: 'Less than 5 years'},
-        {value: 'more-than-5', viewValue: 'More than 5 years'},
-      ],
-    },
-    {
-      name: 'Criminal Background Check',
-      filterOptions: [
-        {value: 'true', viewValue: 'Passed'},
-        {value: 'false', viewValue: 'Failed'},
-      ],
-    },
-  ];
+   
   
 
 }
